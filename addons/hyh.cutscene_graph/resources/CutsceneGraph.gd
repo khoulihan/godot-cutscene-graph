6ddbe464c62a3549529d3986298b66a5f3743fe0
Kevin Houlihan <kevin@crimsoncookie.com>
2021-04-03 22:29:33 +0200

    Added graph type

--- addons/hyh.cutscene_graph/resources/CutsceneGraph.gd
+++ addons/hyh.cutscene_graph/resources/CutsceneGraph.gd
@@ -3,6 +3,7 @@ extends Resource
 
 export (String) var name
 export (String) var display_name
+export (String) var graph_type
 export (Array) var nodes
 export (Resource) var root_node
 export (Array, Resource) var characters
