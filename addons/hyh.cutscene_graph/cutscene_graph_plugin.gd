7cc72c17f9b78b6153983dee048271ec15b418cf
Kevin Houlihan <kevin@crimsoncookie.com>
2021-04-05 18:12:48 +0200

    Workaround for a problem on first run of the project

--- addons/hyh.cutscene_graph/cutscene_graph_plugin.gd
+++ addons/hyh.cutscene_graph/cutscene_graph_plugin.gd
@@ -1,6 +1,8 @@
 tool
 extends EditorPlugin
 
+const CutsceneGraphEditor = preload("scenes/CutsceneGraphEditor.tscn")
+
 var editor
 var editor_button
 
@@ -12,14 +14,48 @@ func _enter_tree():
 	add_custom_type("CutsceneController", "Node", preload("scripts/CutsceneController.gd"), preload("icon_graph_node.svg"))
 	add_custom_type("Cutscene", "Node", preload("scripts/Cutscene.gd"), preload("icon_graph_node.svg"))
 	
+	_create_editor()
+
+
+func _create_editor():
 	editor = preload("scenes/CutsceneGraphEditor.tscn").instance()
 	editor.connect("save_requested", self, "_save_requested")
 	editor_button = add_control_to_bottom_panel(editor, get_plugin_name())
 
 
+# Not sure the cause of this issue, but the editor member is being cleared after
+# the first time the project is run. The control is still present in Godot, but
+# finding it again isn't enough because that has a bunch of references cleared as
+# well. However, finding it and recreating it gets around the issue.
+func _reacquire_editor():
+	print ("Reacquiring graph editor")
+	var root = get_editor_interface().get_base_control()
+	var node_stack = [] + root.get_children()
+	while !node_stack.empty():
+		var current = node_stack.pop_back()
+		if current.name == "CutsceneGraphEditor":
+			editor = current
+			node_stack.clear()
+		else:
+			node_stack = node_stack + current.get_children()
+	if editor == null:
+		print ("Editor not found")
+	else:
+		print ("Editor reacquired")
+		remove_control_from_bottom_panel(editor)
+		if editor != null:
+			editor.free()
+		_create_editor()
+
+
 func apply_changes():
 	if editor != null:
 		editor.perform_save()
+
+
+func clear():
+	if editor != null:
+		_request_clear()
 
 
 func _save_requested(object, path):
@@ -69,8 +105,10 @@ func edit(object):
 
 
 func _request_edit(object, current_resource_path):
-	if editor != null:
-		editor.edit_graph(object, current_resource_path)
+	if editor == null:
+		_reacquire_editor()
+	#if editor != null:
+	editor.edit_graph(object, current_resource_path)
 
 
 func _request_clear():
